2c7fd99c574fc2480aa60861dbb55f0510f7ec2e
Staale Nataas <staale@evokedset.no>
2020-07-09 15:07:23 +0200

    First commit

---
+++ schemas/post_alt.js
@@ -0,0 +1,81 @@
+export default {
+  name: "altPost",
+  title: "Alternative Post",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    },
+    {
+      name: "author",
+      title: "Author",
+      type: "reference",
+      to: { type: "author" },
+    },
+    {
+      name: "mainImage",
+      title: "Main image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "categories",
+      title: "Categories",
+      type: "array",
+      of: [{ type: "reference", to: { type: "category" } }],
+    },
+    {
+      name: "publishedAt",
+      title: "Published at",
+      type: "datetime",
+    },
+    // {
+    //   name: "body",
+    //   title: "Body",
+    //   type: "blockContent",
+    // },
+    {
+      name: "blogPost",
+      title: "Blog Post",
+      type: "document",
+      fields: [
+        // ... other fields ...
+        {
+          name: "body",
+          title: "Body",
+          type: "markdown",
+          options: {
+            minRows: 20,
+          },
+        },
+      ],
+    },
+  ],
+
+  preview: {
+    select: {
+      title: "title",
+      author: "author.name",
+      media: "mainImage",
+    },
+    prepare(selection) {
+      const { author } = selection;
+      return Object.assign({}, selection, {
+        subtitle: author && `by ${author}`,
+      });
+    },
+  },
+};
